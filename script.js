5fc873c41956a01266ffe4b800a7faf80b1caec6
ktkrau <ktkrau@gmail.com>
2022-08-25 10:50:30 -0400

    onclik, mouseover,this

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Página cargada");
+
+function click_editar(){
+    alert("Editando perfil")
+}
+var doble_click = 0;
+function dobleclick_avatar(){
+    doble_click ++;
+    console.log("Ha hecho doble click: "+doble_click+" veces")
+}
+
+function over(){
+    console.log("El mouse está sobre Elena")
+}
+function out(){
+    console.log("El mouse está fuera de Elena")
+}
+function cambieNombre(elemento_h1){ // recibimos el elemento h1
+    console.log(elemento_h1); //La etiqueta la tratamos como objeto
+
+    if(elemento_h1.innerText === "Elena de Troya"){
+        elemento_h1.innerText = "Juana de Arco"
+    }else{
+        elemento_h1.innerText = "Elena de Troya"
+    }
+
+
+   // elemento_h1.innerText = "Juana de Arco"; 
+}
+function cambiaImagen(elemento_img){
+    elemento_img.src = "img/gear.png"
+}
